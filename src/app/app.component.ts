105b2077fabf23c5d123bc82fa20980c2f5afeca
gusfedec <gus.fe.c@gmail.com>
2021-10-01 16:26:45 -0300

    precarga de audio de colores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ export class AppComponent {
   }
   initializeApp() {
     this.platform.ready().then(() => {
+      //Precarga de audios de animales
       this.toastService.preloadAudio(
         'audioBird',
         'assets/audios/animales/bird.mp3'
@@ -72,6 +73,68 @@ export class AppComponent {
       this.toastService.preloadAudio(
         'audioRaton',
         'assets/audios/animales/raton.mp3'
+      );
+
+      //Precarga de audios de colores
+      this.toastService.preloadAudio(
+        'audioYellow',
+        'assets/audios/colores/yellow.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioBlue',
+        'assets/audios/colores/blue.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioGreen',
+        'assets/audios/colores/green.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioPurple',
+        'assets/audios/colores/purple.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioRed',
+        'assets/audios/colores/red.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioAmarilloBra',
+        'assets/audios/colores/amarillo_brasil.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioAzulBra',
+        'assets/audios/colores/azul_brasil.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioVerdeBra',
+        'assets/audios/colores/verde_brasil.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioVioletaBra',
+        'assets/audios/colores/violeta_brasil.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioRojoBra',
+        'assets/audios/colores/rojo_brasil.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioAmarillo',
+        'assets/audios/colores/amarillo.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioAzul',
+        'assets/audios/colores/azul.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioVerde',
+        'assets/audios/colores/verde.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioVioleta',
+        'assets/audios/colores/violeta.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioRojo',
+        'assets/audios/colores/rojo.mp3'
       );
     });
   }
