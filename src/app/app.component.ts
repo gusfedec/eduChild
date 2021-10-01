bdcfac97ab31925186e017c0ac00ec9f1ee4bb74
gusfedec <gus.fe.c@gmail.com>
2021-10-01 16:19:09 -0300

    precarga de audio de animales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,6 +21,58 @@ export class AppComponent {
         'audioCat',
         'assets/audios/animales/cat.mp3'
       );
+      this.toastService.preloadAudio(
+        'audioDog',
+        'assets/audios/animales/dog.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioFish',
+        'assets/audios/animales/fish.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioMouse',
+        'assets/audios/animales/mouse.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioPajaroBra',
+        'assets/audios/animales/pajaro_brasil.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioGatoBra',
+        'assets/audios/animales/gato_brasil.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioPerroBra',
+        'assets/audios/animales/cachorro_brasil.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioPezBra',
+        'assets/audios/animales/pez_brasil.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioRatonBra',
+        'assets/audios/animales/raton_brasil.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioPajaro',
+        'assets/audios/animales/pajaro.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioGato',
+        'assets/audios/animales/gato.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioPerro',
+        'assets/audios/animales/perro.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioPez',
+        'assets/audios/animales/pez.mp3'
+      );
+      this.toastService.preloadAudio(
+        'audioRaton',
+        'assets/audios/animales/raton.mp3'
+      );
     });
   }
 }
